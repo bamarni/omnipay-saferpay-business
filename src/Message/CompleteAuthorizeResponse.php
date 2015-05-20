baed874906ffb142ae4ae39d5c85e3d3b7a8d18d
Bilal Amarni <bamarni@users.noreply.github.com>
2015-05-20 10:14:54 +0200

    returned raw body as message if no authmessage is available

--- src/Message/CompleteAuthorizeResponse.php
+++ src/Message/CompleteAuthorizeResponse.php
@@ -44,12 +44,10 @@ class CompleteAuthorizeResponse extends AbstractResponse
 
     public function getMessage()
     {
-        $authMessage = [];
-
         if (preg_match('/AUTHMESSAGE="([^"]*)"/i', $this->data, $authMessage)) {
             return $authMessage[1];
-        } else {
-            return '';
         }
+
+        return $this->data;
     }
 }
